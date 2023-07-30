ce8a16072f9bc0b1a948a53e822253933935d12a
Abdulafeez Adetunmbi <xplicifire@gmail.com>
2023-07-30 12:37:36 +0100

    removed default validation on generate QR button

--- public/laminar.js
+++ public/laminar.js
@@ -79,7 +79,7 @@ input.addEventListener(
 
         const pattern = /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
 
-        if(input.checkValidity() && pattern.test(input)){
+        if(pattern.test(input)){
            enableButton('initiate');
         };
 
